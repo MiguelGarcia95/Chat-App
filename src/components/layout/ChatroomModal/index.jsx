b8b0d34ec17c5052aee1b3387334357fb4b0c4cc
Miguel <mgarcia95951@gmail.com>
2019-01-02 18:33:26 -0600

    create chatroom modal

---
+++ src/components/layout/ChatroomModal/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {logout} from '../../../redux/actions/authActions';
+// import './style.css';
+
+const ChatroomModal = (props) => {
+  const classes =  true ? 'open' : '';
+  return (
+    <section className={`chatroom-modal ${classes}`}>
+      <section className='modal'>
+        <section className="exit">
+          <i className="fas fa-times fa-2x" onClick={() => {}}></i>
+        </section>
+        <section className="input-group">
+          <input type="text" name='chatroom-name'/>
+        </section>
+        <section className="input-group">
+          <input type='file' name='chatroom-avatar'/>
+        </section>
+        <button className="modal-submit">Create</button>
+      </section>
+    </section>
+  )
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    // logout: () => dispatch(logout())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(ChatroomModal);
