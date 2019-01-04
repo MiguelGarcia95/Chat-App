48a85a099d9ac11123d172972903d7e04c8dbe94
Miguel <mgarcia95951@gmail.com>
2019-01-04 16:49:43 -0600

    create chatroom template

--- src/components/layout/ChatroomSection/index.jsx
+++ src/components/layout/ChatroomSection/index.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './style.css';
 
-const ChatroomSection = (props) => {
-  return (
-    <section className="chatroom-section first">
-      <section className="section-name"><i className="fas fa-chevron-down"></i> <p>Game Recommendations</p></section>
-      <section className="channels">
-        <section className="channel first active"><i className="fas fa-hashtag"></i> <p>rpg</p></section>
-        <section className="channel"><i className="fas fa-hashtag"></i> <p>stg</p></section>
-        <section className="channel"><i className="fas fa-hashtag"></i> <p>jrpg</p></section>
-        <section className="channel"><i className="fas fa-hashtag"></i> <p>metroidvania</p></section>
-        <section className="channel"><i className="fas fa-hashtag"></i> <p>retro</p></section>
-      </section>
-    </section> 
-  )
+class ChatroomSection extends React.Component {
+  render() {
+    return (
+      <section className="chatroom-section first">
+        <section className="section-name"><i className="fas fa-chevron-down"></i> <p>Game Recommendations</p></section>
+        <section className="channels">
+          <section className="channel first active"><i className="fas fa-hashtag"></i> <p>rpg</p></section>
+          <section className="channel"><i className="fas fa-hashtag"></i> <p>stg</p></section>
+          <section className="channel"><i className="fas fa-hashtag"></i> <p>jrpg</p></section>
+          <section className="channel"><i className="fas fa-hashtag"></i> <p>metroidvania</p></section>
+          <section className="channel"><i className="fas fa-hashtag"></i> <p>retro</p></section>
+        </section>
+      </section> 
+    )
+  }
 };
 
 export default ChatroomSection
