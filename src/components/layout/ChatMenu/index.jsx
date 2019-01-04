f9ba023ee26f4ca1a006a99ad51d84bdf00ae8a2
Miguel <mgarcia95951@gmail.com>
2019-01-04 16:28:47 -0600

    create chatroom template

--- src/components/layout/ChatMenu/index.jsx
+++ src/components/layout/ChatMenu/index.jsx
@@ -3,22 +3,16 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 import Userbar from '../Userbar';
+import ChatroomSection from '../ChatroomSection/';
 
 const ChatMenu = ({user}) => {
   return (
     <section className="menu-bar chatroom-menu">
       <Userbar user={user} />
       {/* Chatroom section */}
-      <section className="chatroom-section first">
-        <section className="section-name"><i className="fas fa-chevron-down"></i> <p>Game Recommendations</p></section>
-        <section className="channels">
-          <section className="channel first active"><i className="fas fa-hashtag"></i> <p>rpg</p></section>
-          <section className="channel"><i className="fas fa-hashtag"></i> <p>stg</p></section>
-          <section className="channel"><i className="fas fa-hashtag"></i> <p>jrpg</p></section>
-          <section className="channel"><i className="fas fa-hashtag"></i> <p>metroidvania</p></section>
-          <section className="channel"><i className="fas fa-hashtag"></i> <p>retro</p></section>
-        </section>
-      </section> 
+      <ChatroomSection />
+      
+
       {/* Chatroom section End */}
     </section>
   )
