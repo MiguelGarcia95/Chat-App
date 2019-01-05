5ee5753335fac1152fd468a99966accbf2f3c7ee
Miguel <mgarcia95951@gmail.com>
2019-01-04 19:03:08 -0600

    finished chatroom template

--- src/components/layout/ChatPanel/index.jsx
+++ src/components/layout/ChatPanel/index.jsx
@@ -6,73 +6,38 @@ class ChatPannel extends React.Component {
   render () {
     return (
       <section className="chat-panel">
-        <nav className="panel-header">
-          <i className="fas fa-plus fa-2x"></i>
-          <span className="panel-box first"><p className="active">All</p></span>
-          <span className="panel-box"><p>Online</p></span>
-          <span className="panel-box"><p>Pending</p></span>
+        <nav class="chatroom-header">
+          <i class="fas fa-hashtag"></i> <p>RPG - Give us your favorite games of the genre.</p>
         </nav>
 
-        <section className="friend-list">
-          <section className="container">
-            <section className="row-header">
-              <section className="username"><p>Username</p></section>
-              <section className="status"><p>Status</p></section>
+        <sections className="comments">
+
+          <section className="comment-container">
+            <section className="avatar">
+              <img src="https://uploads.disquscdn.com/images/e3a640f5ae1bf628977c502faea357f8f91eb619e66b30d00c179401f180d39b.jpg" alt="avatar" />
             </section>
 
-            <section className="row">
-              <section className="username">
-                <section className="avatar"><img src="https://uploads.disquscdn.com/images/e3a640f5ae1bf628977c502faea357f8f91eb619e66b30d00c179401f180d39b.jpg" alt="avatar" /></section>
-                <section className="name"><p>Holy Coffee Cup</p></section>
+            <section className="content">
+              <section className="top">
+                <section className="username"><p>Vortt</p></section>
+                <section className="date"><p>Today @ 11:50 am</p></section>
               </section>
-              <section className="status">
-                <section className="status-light"><section className="light online"></section></section>
-                <section className="info">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis </p>
+              <section className="bottom">
+                <section className="comment">
+                  <p>NieR is an amazing game. The gameplay may not be praiseworthy, but the world is amazing. It makes you care about the characters.</p>
                 </section>
               </section>
             </section>
-            <section className="row">
-              <section className="username">
-                <section className="avatar"><img src="https://uploads.disquscdn.com/images/e3a640f5ae1bf628977c502faea357f8f91eb619e66b30d00c179401f180d39b.jpg" alt="avatar" /></section>
-                <section className="name"><p>Holy Coffee Cup</p></section>
-              </section>
-              <section className="status">
-                <section className="status-light"><section className="light offline"></section></section>
-                <section className="info">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis </p>
-                </section>
-              </section>
-            </section>
-            <section className="row">
-              <section className="username">
-                <section className="avatar"><img src="https://uploads.disquscdn.com/images/e3a640f5ae1bf628977c502faea357f8f91eb619e66b30d00c179401f180d39b.jpg" alt="avatar" /></section>
-                <section className="name"><p>Holy Coffee Cup</p></section>
-              </section>
-              <section className="status">
-                <section className="status-light"><section className="light online"></section></section>
-                <section className="info">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis </p>
-                </section>
-              </section>
-            </section>
-            <section className="row">
-              <section className="username">
-                <section className="avatar"><img src="https://uploads.disquscdn.com/images/e3a640f5ae1bf628977c502faea357f8f91eb619e66b30d00c179401f180d39b.jpg" alt="avatar" /></section>
-                <section className="name"><p>Holy Coffee Cup</p></section>
-              </section>
-              <section className="status">
-                <section className="status-light"><section className="light pending"></section></section>
-                <section className="info">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis </p>
-                </section>
-              </section>
-            </section>
-
-
-
           </section>
-        </section>
+
+        </sections>
+ 
+         <section class="chat-box">
+           <section class="input-group">
+             <textarea name="comment" id="" ></textarea>
+           </section>
+         </section>
+
       </section>
     )
   }
