f3a28fb23131baa761a70655a86df9e27f1eb378
Miguel <mgarcia95951@gmail.com>
2019-01-04 22:22:20 -0600

    finished chatroom template

--- src/components/layout/ChatMenu/index.jsx
+++ src/components/layout/ChatMenu/index.jsx
@@ -5,19 +5,27 @@ import './style.css';
 import Userbar from '../Userbar';
 import ChatroomSection from '../ChatroomSection/';
 
-const ChatMenu = ({user}) => {
-  const chatroomChannel = ['rpg', 'stg', 'jrpg', 'metroidvania', 'retro'];
-  return (
-    <section className="menu-bar chatroom-menu">
-      <Userbar user={user} />
-      {/* Chatroom section */}
-      <ChatroomSection sectionName={'Game Recommendations'} channels={chatroomChannel} />
-      <ChatroomSection sectionName={'Discussion'} channels={chatroomChannel} />
-      <ChatroomSection sectionName={'Ranting'} channels={chatroomChannel} />
-
-      {/* Chatroom section End */}
-    </section>
-  )
+class ChatMenu extends React.Component {
+  state = {
+    user: this.props.user
+  }
+  render() {
+    // pull all chatroom sections,
+    // pull all channels within the chatroom, since only one will be active,
+    // sort all of the channels with their approriate section and pass them thru
+    const chatroomChannel = ['rpg', 'stg', 'jrpg', 'metroidvania', 'retro'];
+    const {user} = this.state;
+    return (
+      <section className="menu-bar chatroom-menu">
+        <Userbar user={user} />
+        {/* Chatroom section */}
+        <ChatroomSection sectionName={'Game Recommendations'} channels={chatroomChannel} />
+        <ChatroomSection sectionName={'Discussion'} channels={chatroomChannel} />
+        <ChatroomSection sectionName={'Ranting'} channels={chatroomChannel} />
+        {/* Chatroom section End */}
+      </section>
+    )
+  }
 }
 
 ChatMenu.propTypes = {
