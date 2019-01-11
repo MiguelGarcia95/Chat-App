9f626afef391393471a4938c0ccda24a587455eb
Miguel <mgarcia95951@gmail.com>
2019-01-10 19:49:47 -0600

    organize channels by category id

--- src/components/layout/ChatroomChannel/index.jsx
+++ src/components/layout/ChatroomChannel/index.jsx
@@ -2,32 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
-class ChatroomChannel extends React.Component {
-  state = {
-    channel: this.props.channel,
-    categoryId: this.props.categoryId
-  }
-  doesItbelongHere = (channel) => {
-    if (channel.channelData.categoryId === this.state.categoryId) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  render() {
-    const {channel, categoryId} = this.state;
-    console.log(channel.channelData)
-    if (this.doesItbelongHere(channel)) {
-      return (
-        <section className={`channel ${categoryId}`} key={channel.id}><i className="fas fa-hashtag"></i> <p>{channel.channelData.channelName}</p></section>
-      )
-    } else {
-      return (
-        <p>Loading</p>
-      )
-    }
-  }
+const ChatroomChannel = (props) => {
+  const {channel, categoryId} = props;
+  return (
+    <section className={`channel ${categoryId}`} key={channel.id}><i className="fas fa-hashtag"></i> <p>{channel.channelData.channelName}</p></section>
+  )
 }
 
 // ChatroomChannel.propTypes = {
