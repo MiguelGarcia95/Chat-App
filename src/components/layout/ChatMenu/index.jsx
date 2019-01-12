b319961cd4a34d8863158bca5b73c612cf46d53f
Miguel <mgarcia95951@gmail.com>
2019-01-12 15:01:56 -0600

    set current channel

--- src/components/layout/ChatMenu/index.jsx
+++ src/components/layout/ChatMenu/index.jsx
@@ -3,40 +3,60 @@ import PropTypes from 'prop-types';
 import './style.css';
 import ChatroomSection from '../ChatroomSection/';
 
-class ChatMenu extends React.Component {
-  // state = {
-  //   user: this.props.user,
-  //   chatroom: this.props.chatroom,
-  //   categories: this.props.categories
-  // }
-
-  displayCategories = (categories) => {
-    return categories.map((category) => {
-      return (
-        <ChatroomSection 
-          key={category.id} 
-          sectionName={category.category.categoryName}
-          category={category}
-          isUserAdmin={this.props.isUserAdmin}
-          toggleChannelModal={this.props.toggelChannel}
-          newChannelMade={this.props.newChannelMade}
-          toggleChannelState={this.props.toggleChannelState}
-          setCurrentChannelId={this.props.setCurrentChannelId}
-          currentChannelId={this.props.currentChannelId}
-          changeChat={this.props.changeChat}
-        />
-      )
-    })
-  }
-
-  render() {
+const displayCategories = (props) => {
+  return props.categories.map((category) => {
     return (
-      <section className="menu-bar chatroom-menu">
-        {this.displayCategories(this.props.categories)}
-      </section>
+      <ChatroomSection 
+        key={category.id} 
+        sectionName={category.category.categoryName}
+        category={category}
+        isUserAdmin={props.isUserAdmin}
+        toggleChannelModal={props.toggelChannel}
+        newChannelMade={props.newChannelMade}
+        toggleChannelState={props.toggleChannelState}
+        setCurrentChannelId={props.setCurrentChannelId}
+        currentChannelId={props.currentChannelId}
+        changeChat={props.changeChat}
+      />
     )
-  }
+  })
 }
+
+const ChatMenu = (props) => {
+  return (
+    <section className="menu-bar chatroom-menu">
+      {displayCategories(props)}
+    </section>
+  )
+}
+
+// class ChatMenu extends React.Component {
+//   displayCategories = (categories) => {
+//     return categories.map((category) => {
+//       return (
+//         <ChatroomSection 
+//           key={category.id} 
+//           sectionName={category.category.categoryName}
+//           category={category}
+//           isUserAdmin={this.props.isUserAdmin}
+//           toggleChannelModal={this.props.toggelChannel}
+//           newChannelMade={this.props.newChannelMade}
+//           toggleChannelState={this.props.toggleChannelState}
+//           setCurrentChannelId={this.props.setCurrentChannelId}
+//           currentChannelId={this.props.currentChannelId}
+//           changeChat={this.props.changeChat}
+//         />
+//       )
+//     })
+//   }
+//   render() {
+//     return (
+//       <section className="menu-bar chatroom-menu">
+//         {this.displayCategories(this.props.categories)}
+//       </section>
+//     )
+//   }
+// }
 
 ChatMenu.propTypes = {
   user: PropTypes.object.isRequired
