649230ea4d2908e95989c80adc925eebe07d039c
Miguel <mgarcia95951@gmail.com>
2019-01-11 18:37:15 -0600

    set current channel

--- src/redux/reducers/chatroomReducer.js
+++ src/redux/reducers/chatroomReducer.js
@@ -91,12 +91,50 @@ const chatroomReducer = (state = initialState, action) => {
         chatroomError: action.payload.chatroomError,
         chatroomCategories: action.payload.chatroomCategories
       }
-    case actionTypes.GET_CATEGORY_CHANNELS:
-    // console.log(`Channels coming in: ${action.payload.chatroomChannels}`);
-    // console.log(`Channels already in array ${state.chatroomChannels}`);
+      case actionTypes.GET_CATEGORY_CHANNELS:
+      let channelCategories = state.chatroomChannels;
+      //cycle thru all incoming categories. 
+      //if category is in state, replace it.
+      action.payload.chatroomChannels.forEach(category => {
+        let notInState = true;
+        // console.log('new')
+        // console.log(channelCategories.length)
+        channelCategories.forEach(channelCategory => {
+          console.log(channelCategory.id)
+          console.log(category.id)
+          if (channelCategory.id == category.id) {
+            category = channelCategory;
+            notInState = false;
+          }
+        })
+        if (notInState) {
+          // console.log('added new one')
+          // console.log(category)
+          channelCategories.push(category)
+        }
+      })
+
+      // channels.forEach(category => {
+      //   if (channels.length > 0) {
+      //     channels.forEach(channel => {
+      //       if (channel.id === category.id) {
+      //         // channel = category
+      //         console.log(channel, category)
+      //       }
+      //     })
+      //   } else {
+      //     // channels.push(category)
+      //   }
+      //   // console.log(category.id)
+      // })
+
+      // console.log(`Channels coming in: `, action.payload.chatroomChannels);
+      // console.log(`Channels already in array: `, state.chatroomChannels);
+      // console.log('Channels: ', channelCategories);
       return {
         ...state,
         chatroomError: action.payload.chatroomError,
+        // chatroomChannels: channelCategories
         chatroomChannels: state.chatroomChannels.concat(action.payload.chatroomChannels)
       }
     case actionTypes.GET_CATEGORY_CHANNELS_ERROR:
