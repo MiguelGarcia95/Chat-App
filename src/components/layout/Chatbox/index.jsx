462ba50645fed85e0da4b8983ebfbaab9c9932cb
Miguel <mgarcia95951@gmail.com>
2019-01-12 20:51:38 -0600

    create channel comment

--- src/components/layout/Chatbox/index.jsx
+++ src/components/layout/Chatbox/index.jsx
@@ -4,11 +4,21 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 class Chatbox extends React.Component {
+  state = {
+    content: ''
+  }
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('test')
+  }
   render() {
     return (
       <section className="chat-box">
         <section className="input-group">
-          <textarea name="comment" id="" ></textarea>
+          <form onSubmit={this.onFormSubmit}>
+            <input name="comment" id="" />
+            {/* <textarea name="comment" id="" ></textarea> */}
+          </form>
         </section>
       </section>
     )
