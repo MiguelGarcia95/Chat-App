85362585e8dc2105611f2fd25954c34bc466ee5a
Miguel <mgarcia95951@gmail.com>
2019-01-13 15:54:40 -0600

    display comments

--- src/components/layout/Comments/index.jsx
+++ src/components/layout/Comments/index.jsx
@@ -4,14 +4,15 @@ import PropTypes from 'prop-types';
 import Comment from '../Comment/';
 import './style.css';
 
-const displayComments = (props) => {
-  console.log(props.comments)
+const displayComments = ({comments}) => {
+  if (comments.length > 0) {
+    return comments.map(comment => <Comment key={comment.id} comment={comment.comment} />)
+  }
 }
 
 const Comments = (props) => {
   return (
     <section className="comments">
-      <Comment />
       {displayComments(props)}
     </section>
   )
