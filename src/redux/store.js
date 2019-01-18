87691670658dea09f20d06a693df8cd522e04931
Miguel <mgarcia95951@gmail.com>
2019-01-17 19:09:01 -0600

    clean

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,6 @@ import {reactReduxFirebase, getFirebase} from 'react-redux-firebase';
 import {reduxFirestore, getFirestore} from 'redux-firestore';
 import {createStore, compose, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
-// import firebase from 'firebase';
-// import firebase from 'firebase/app';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers/rootReducer';
